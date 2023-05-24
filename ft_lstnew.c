d43faed38ac6e90691c8dd6d6d5d3f0ee9585929
Letnys <120330424+Letnys@users.noreply.github.com>
2023-05-24 22:53:59 +0200

    first push

---
+++ ft_lstnew.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstnew.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: egeorgel <egeorgel@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/17 15:11:32 by egeorgel          #+#    #+#             */
+/*   Updated: 2023/02/18 18:52:51 by egeorgel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+#include "libft.h"
+
+t_list	*ft_lstnew(void *content)
+{
+	t_list	*lst;
+
+	lst = malloc(sizeof(*lst));
+	if (!lst)
+		return (0);
+	lst->content = content;
+	lst->next = NULL;
+	return (lst);
+}
+6
